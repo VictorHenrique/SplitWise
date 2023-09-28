6869ef3f9b9939a5029ea86f33fbf7bde8326549
Victor Henrique de Sa Silva <victor.hss@hotmail.com>
2023-09-28 19:02:45 -0300

    remove node_modules and .expo and adding style files

---
+++ components/login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import styles from '../styles.js'
+
+const LoginScreen = ({navigation, accounts}) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => { if (accounts[username] === password) {
+            navigation.navigate('Home', {username});
+        } else {
+            alert('Usuario e/ou senha invalido');
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.label}>Usuario:</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(text) => setUsername(text)}
+                value={username}
+            />
+            <Text style={styles.label}>Senha:</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(text) => setPassword(text)}
+                value={password}
+                secureTextEntry={true}
+            />
+            <Button title='Login' style={styles.button} onPress={handleLogin}/>
+            <View style={styles.verticalSpace}/>
+            <Button title='Cadastrar' style={styles.button} onPress={() => navigation.navigate('Register')}/>
+        </View>
+    )
+};
+
+export default LoginScreen;
---
+++ components/styles/theme.js
@@ -0,0 +1,36 @@
+import colors from './color.js'
+
+const theme = {
+    md_sys_color_primary: colors.md_sys_color_primary_dark,
+    md_sys_color_on_primary: colors.md_sys_color_on_primary_dark,
+    md_sys_color_primary_container: colors.md_sys_color_primary_container_dark,
+    md_sys_color_on_primary_container: colors.md_sys_color_on_primary_container_dark,
+    md_sys_color_secondary: colors.md_sys_color_secondary_dark,
+    md_sys_color_on_secondary: colors.md_sys_color_on_secondary_dark,
+    md_sys_color_secondary_container: colors.md_sys_color_secondary_container_dark,
+    md_sys_color_on_secondary_container: colors.md_sys_color_on_secondary_container_dark,
+    md_sys_color_tertiary: colors.md_sys_color_tertiary_dark,
+    md_sys_color_on_tertiary: colors.md_sys_color_on_tertiary_dark,
+    md_sys_color_tertiary_container: colors.md_sys_color_tertiary_container_dark,
+    md_sys_color_on_tertiary_container: colors.md_sys_color_on_tertiary_container_dark,
+    md_sys_color_error: colors.md_sys_color_error_dark,
+    md_sys_color_error_container: colors.md_sys_color_error_container_dark,
+    md_sys_color_on_error: colors.md_sys_color_on_error_dark,
+    md_sys_color_on_error_container: colors.md_sys_color_on_error_container_dark,
+    md_sys_color_background: colors.md_sys_color_background_dark,
+    md_sys_color_on_background: colors.md_sys_color_on_background_dark,
+    md_sys_color_surface: colors.md_sys_color_surface_dark,
+    md_sys_color_on_surface: colors.md_sys_color_on_surface_dark,
+    md_sys_color_surface_variant: colors.md_sys_color_surface_variant_dark,
+    md_sys_color_on_surface_variant: colors.md_sys_color_on_surface_variant_dark,
+    md_sys_color_outline: colors.md_sys_color_outline_dark,
+    md_sys_color_inverse_on_surface: colors.md_sys_color_inverse_on_surface_dark,
+    md_sys_color_inverse_surface: colors.md_sys_color_inverse_surface_dark,
+    md_sys_color_inverse_primary: colors.md_sys_color_inverse_primary_dark,
+    md_sys_color_shadow: colors.md_sys_color_shadow_dark,
+    md_sys_color_surface_tint: colors.md_sys_color_surface_tint_dark,
+    md_sys_color_outline_variant: colors.md_sys_color_outline_variant_dark,
+    md_sys_color_scrim: colors.md_sys_color_scrim_dark, 
+}
+
+export default theme;
